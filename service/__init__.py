0ae911cf6f684870d7a131381d70120dd507b8b7
mre0979 <mre0979@gmail.com>
2025-06-07 10:10:04 -0400

    Added security headers

--- service/__init__.py
+++ service/__init__.py
@@ -6,12 +6,16 @@ and SQL database
 """
 import sys
 from flask import Flask
+from flask_talisman import Talisman
 from service import config
 from service.common import log_handlers
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+
+# Initialize Talisman with the Flask app
+talisman = Talisman(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
@@ -29,6 +33,7 @@ app.logger.info(70 * "*")
 
 try:
     models.init_db(app)  # make our database tables
+
 except Exception as error:  # pylint: disable=broad-except
     app.logger.critical("%s: Cannot continue", error)
     # gunicorn requires exit code 4 to stop spawning workers when they die
